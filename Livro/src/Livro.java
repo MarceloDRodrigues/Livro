a8d162c5eb3170166a55b57b810870904f249980
MarceloDRodrigues <101276172+MarceloDRodrigues@users.noreply.github.com>
2022-12-01 11:15:29 -0300

    final

--- Livro/src/Livro.java
+++ Livro/src/Livro.java
@@ -6,26 +6,26 @@ public class App {
         Scanner escaneador = new Scanner(System.in);
         Random dado = new Random();
         System.out.println(
-                "Esse é um jogo que conta a historia de Math Yeus em sua provavel desventura em um mundo um tanto quanto esquisito.");
-        System.out.println("Essa história interativa irá contar com o auxilio do jogador na tomada de decisões.");
+                "Esse é um livro que conta a historia de Math Yeus em sua provável desventura em um mundo um tanto quanto esquisito.");
+        System.out.println("Essa história interativa irá contar com o auxilio do usuário na tomada de decisões.");
         System.out.println(
-                "Essas escolhas serão tomadas por meio de decisões binários, com o digito O ou o digito 1, indicado como se segue cada um respectivamente");
+                "Essas escolhas serão tomadas por meio de decisões binários, com o digito O ou o digito 1, indicados respectivamente");
         System.out.println("----------------------------------------------------------------------");
         System.out.println(
-                "Math Yeus acorda em um simples local chamado Vila Cesamo, sem ter qualquer outra memoria do que fez de sua vida até então.");
+                "Math Yeus acorda em um simples local chamado Villace zamo, sem ter qualquer outra memoria do que fez de sua vida até então.");
         System.out.println(
-                "Ao observar o local, encontra dois possiveis destinos para se situar sobre onde ele se encontra. A direita, tem-se um lugar semelhante a um enorme castelo, no qual a frente pasta uma vaca de tamanho gigantesco");
+                "Ao observar o local, encontra dois possiveis destinos para se situar sobre onde ele se encontra. A direita, tem-se um lugar semelhante a um enorme castelo, no qual a frente pasta uma vaca gigantesca");
         System.out.println(
-                "A esquerda, vê que há algo semelhante a uma guilda, o qual estranhamente ele sabe da existência.");
+                "A esquerda, vê que há algo semelhante a uma guilda, com alguns avisos na parede, mas voce não consegue ler.");
         System.out.println("Digite 1 para ir ao Castelo e 0 para ir a Guilda!!!!");
         int escolha = escaneador.nextInt();
 
         if (escolha == 1) {
-            boolean castelo = true;
+      
             System.out.println(
-                    "Voce vai para o castelo e na frente voce observa novamente a vaca, seus musculos amendrontadores... E se pergunta como ela ficou desse jeito");
+                    "Voce vai para o castelo e na frente voce observa novamente a vaca, com músculos inacreditavelmente grandes. Por fim, voce se pergunta como ela ficou desse jeito.");
             System.out.println(
-                    "A vaca come a grama com uma intensidade assustadora e voce cogita comer a grama também. Voce irá comer a grama? 1 para sim e 0 para não.");
+                    "A vaca come a grama com uma intensidade assustadora e voce, por alguma razão, cogita comer a grama também. Voce irá comer a grama? 1 para sim e 0 para não.");
             int escolha_a1 = escaneador.nextInt();
             if (escolha_a1 == 1) {
                 System.out.println("Voce come a grama e nada acontece. Por acaso voce é idiota?");
@@ -35,35 +35,37 @@ public class App {
             } else {
                 System.out.println("Voce ignora a enorme vaca e prossegue para o castelo.");
                 System.out.println(
-                        "Chegando ao castelo voce encontra três homens grandes e com aparências imponentes. Ao iniciar uma conversa com eles, eles se apreseentam como Joffrey, o mago do Reino, o Caulo Muzy, o bibliotecário e o próprio Rei, Cristan");
+                        "Chegando ao castelo voce encontra três homens grandes e com aparências imponentes. Ao iniciar uma conversa com eles, eles se apreseentam como Joffrey, o mago do Reino, o Caulo Muzy, o bibliotecário e o próprio Rei, Cristan.");
                 System.out.println(
                         "Caulo muzy convida voce para visitar a biblioteca, porém Joffrey, com muita bondade, te chama para visitar sua sala mágica.");
-                System.out.println("Digite 1 para ir acompanhar Caulo e 0 para seguri Joffrey");
+                System.out.println("O rei, no entando, parece pouco se importar com voce. Apenas dá as costas e vai embora para uma sala com um trono.");
+                System.out.println("Digite 1 para acompanhar Caulo e 0 para seguir Joffrey!!!!");
                 int escolha_a2 = escaneador.nextInt();
                 if (escolha_a2 == 1) {
                     System.out.println(
-                            "Ao caminho da biblioteca, vocês são teletransportados para uma floresta branca que voce nunca viu antes, e voces encaram uma figura semelhante a um Samurai dos tempos antigos do japão");
+                            "No caminho da biblioteca, vocês são teletransportados para uma floresta branca que voce nunca viu antes, e voces encaram uma figura semelhante a um Samurai dos tempos antigos do japão.");
                 } else {
                     System.out.println(
-                            "Voce segue Joffrey até a sala dele, chegando lá, abre-se uma porta mágica no centro da sala e de lá sai um mago que parece ser ainda mais implacável que o Joffrey");
+                            "Voce segue Joffrey até a sala dele, chegando lá, abre-se uma porta mágica no centro da sala e de lá sai um mago - que parece ser ainda mais implacável que o Joffrey.");
 
                 }
             }
         } else {
-            boolean guilda = true;
+           
             System.out.println(
-                    "Voce vai para a guilda e encontra um aviso rasgado sobre tomar cuidado com alguma coisa que voce so consegue identificar que tinham 5 letras");
-            int dado_p = dado.nextInt(19) + 1;
-            if (dado_p >= 10) {
+                    "Voce vai para a guilda e encontra um aviso rasgado sobre tomar cuidado com alguma coisa que voce só consegue identificar que haviam 5 letras ali.");
+            int d20 = dado.nextInt(19) + 1;
+            if (d20 >= 10) {
                 System.out.println(
-                        "Voce entra na guilda e encontra um ambiente caótico, com diversos homens brigando e bebendo. Mas uma figura se destaca no ambiente, no balcão uma mulher loira e com músculos aparentes. Voce vai falar com ela e se apresenta. O nome dela é Karine, e ela explica que ela é líder da guilda");
-                System.out.println("Digite 1 para acompanhar a Karine e 0 para não");
+                        "Voce entra na guilda e encontra um ambiente caótico, com diversos homens brigando e bebendo. Mas uma figura se destaca no ambiente, no balcão há uma mulher loira e com músculos aparentes, a qual parece impor respeito naquele local. Voce vai falar com ela e se apresenta. O nome dela é Karine, e ela explica que ela é líder da guilda");
                 System.out.println(
-                        "Karine fica curiosa com sua presença e te convida para uma luta 'na brincadeira' na arena. Voce aceita? 1 para sim e 2 para não");
+                        "Karine fica curiosa com sua presença e te convida para uma luta 'na brincadeira' na arena. Voce aceita? 1 para sim e 0 para não");
+             
+
                 int escolha_b1 = escaneador.nextInt();
                 if (escolha_b1 == 1) {
                     System.out.println(
-                            "Voces chegam em um lugar semelhante a um coliseu romano sem público, Karine diz para voce se preparar e ir para o lado oposto da arena ");
+                            "Voces chegam em um lugar semelhante a um coliseu romano sem público, Karine diz para voce se preparar e ir para o lado oposto da arena.");
                     System.out.println(
                             "Voce não entende bem o que aconteceu, mas sua cabeça está separada do seu corpo.");
                 } else {
@@ -71,11 +73,11 @@ public class App {
                             "Os outros homens ficam irritados por voce ter rejeitado o convite de Karine, voce é arremessado contra a parede e apaga.");
                 }
             } else {
-                System.out.println("Voce toma uma bicuda da porta e começa a voar indefinidamente.");
+                System.out.println(
+                        "Voce toma uma bicuda de algum bêbado através da porta e começa a voar indefinidamente.");
             }
         }
 
         escaneador.close();
     }
-}
 }
